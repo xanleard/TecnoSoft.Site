7f7e3d0ae3565ebf7cd28d2861a123369f375d71
Rudy Cardona <rudy.cardona@grupoleitz.com>
2022-01-06 10:46:59 -0600

    Add project files.

---
+++ TecnoSoft.Core/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TecnoSoft.DataBase;
+
+namespace TecnoSoft.Core
+{
+    public class ProductRepository
+    {
+        public Product GetProducto()
+        {
+            return new Product { Id = 1, Cantidad = 2, Estado = "Registrado"};
+        }
+    }
+}
---
+++ TecnoSoft.DataBase/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace TecnoSoft.DataBase
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public string Estado { get; set; }
+
+    }
+}
